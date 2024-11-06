607daca1279d1d71409466dbc39a0462b4a6b75b
GhiathAlshammaa <g.alshammaa@rewe-group.at>
2024-11-06 03:48:52 +0100

    update configs

--- src/environment/environment.prod.ts
+++ src/environment/environment.prod.ts
@@ -1,5 +1,6 @@
 export const environment = {
     production: true,
+    baseHref: '/BarrierMonitoringApp/',
     googleMapsApiKey: 'AIzaSyCCh_TInHswTYastilofuDnPYj1VlcF94U'
   };
   
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
-// Ensure the 'browser' variable is defined globally
-(window as any).browser = (window as any).browser || (window as any).chrome || window;
 
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
+
+
+
+// Ensure the 'browser' variable is defined globally
+(window as any).browser = (window as any).browser || (window as any).chrome || window;
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
